048810c567f75ecdaeaf76ceeb5af3e18bf88006
Octavio Lara <octavioasapchi@gmail.com>
2023-03-31 14:57:20 -0400

    [:sparkles: ADD] blog fix

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Blog = require("../models/Blog");
+const Blog = require("../models/blog");
 
 // Get all blogs
 router.get("/", async (req, res) => {
